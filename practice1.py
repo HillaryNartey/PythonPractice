2408cbaa5662949092db3c67ccd040c8d9ca7aea
Hillary <hillarynartey@gmail.com>
2023-08-02 22:25:29 +0100

    lab2

--- practice1.py
+++ practice1.py
@@ -1,11 +1,44 @@
-#lab1 
-print('hello, world')
+# #lab1 
+# print('hello, world')
 
-#lab2
-#create 2 variables for my first name and second name and print this variables in the console
+# #lab2
+# #create 2 variables for my first name and second name and print this variables in the console
 
 
-first_name = "Hillary"
-surname = "Nartey"
+# first_name = "Hillary"
+# surname = "Nartey"
+# D_O_B = 14
+# M_O_B = 12
+# Y_O_B = 1788
 
-print("My name is " + first_name +" "+ surname)
+# print("My name is " + first_name +" "+ surname +"." + "My date of birth is " + str(D_O_B) + "/" +str(M_O_B) + "/" + str(Y_O_B))
+
+# from datetime import datetime
+
+# now = datetime.now()
+
+# current_time = now.strftime("%H:%M:%S")
+# print("My Current Time =", current_time)
+
+# #print("My current time is" "%H:%M:%S")
+
+# print(str(D_O_B + 10))
+
+
+
+
+# #create an input that the user will request for the name
+
+# # using input() to take user input
+# D_O_B = input('Enter str(D_O_B + 2): ')
+
+# print('You Entered:',str(int(D_O_B) + 2))
+
+# #print('Data type of D_O_B:', type(D_O_B))
+
+
+def greetings():
+    greet = "hello world"
+    return greet
+
+print(greetings())
