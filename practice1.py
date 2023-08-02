36c354a3c5ce6e0a8bd5d61c9bd9e69fc72c9e3e
Hillary <hillarynartey@gmail.com>
2023-08-02 21:05:49 +0100

    lab1

---
+++ practice1.py
@@ -0,0 +1,11 @@
+#lab1 
+print('hello, world')
+
+#lab2
+#create 2 variables for my first name and second name and print this variables in the console
+
+
+first_name = "Hillary"
+surname = "Nartey"
+
+print("My name is " + first_name +" "+ surname)
